2e38206bd483f7c0495ffaeb10df3f0e8a459123
Agent-Perry-the-Platypus <72860524+Agent-Perry-the-Platypus@users.noreply.github.com>
2020-10-23 08:38:03 +0300

    Модифікація структури таблиць бази даних засобами SQL

---
+++ Лаб_3/SQLQuery.sql
@@ -0,0 +1,11 @@
+﻿ALTER TABLE СТО
+ADD Рейтинг INT NULL;
+
+ALTER TABLE Автомобіль
+DROP COLUMN Колір;
+
+ALTER TABLE Клієнт
+ALTER COLUMN Будинок INT NULL;
+
+ALTER TABLE СТО
+ADD CHECK (Рейтинг<=5 AND Рейтинг>=0);
