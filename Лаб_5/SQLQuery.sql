f9cba55308abe8f7abe84b0014efb2981d7c7877
Agent-Perry-the-Platypus <72860524+Agent-Perry-the-Platypus@users.noreply.github.com>
2020-10-28 15:42:39 +0200

    операцій реляційної алгебри засобами SQL

---
+++ Лаб_5/SQLQuery.sql
@@ -0,0 +1,17 @@
+﻿SELECT *
+  FROM [Ремонт_автомобілів].[dbo].[Клієнт_1];
+
+SELECT *
+  FROM [Ремонт_автомобілів].[dbo].[Клієнт_2];
+
+SELECT * FROM Клієнт_1
+	UNION SELECT * FROM Клієнт_2
+
+SELECT * FROM Клієнт_1
+INTERSECT SELECT * FROM Клієнт_2
+
+SELECT * FROM Клієнт_1
+WHERE Клієнт_1.[Код клієнта] NOT IN (SELECT [Код клієнта] FROM Клієнт_2);
+
+SELECT * FROM Клієнт_1
+CROSS JOIN Клієнт_2;
