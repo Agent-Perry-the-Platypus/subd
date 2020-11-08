5fd9bc775d05210e5cc68aa0ab0e0264f2d9b2a1
Agent-Perry-the-Platypus <72860524+Agent-Perry-the-Platypus@users.noreply.github.com>
2020-11-08 19:55:23 +0200

    Розробка та застосування транзакцій та тригерів

---
+++ Лаб_11/Lab_11.sql
@@ -0,0 +1,9 @@
+﻿USE Ремонт_автомобілів;
+
+BEGIN TRANSACTION 
+INSERT INTO Деталь VALUES (12, N'Шини', 499.00,N'Україна'); 
+INSERT INTO Деталь VALUES (13, N'Диски', 441.00,N'Японія'); 
+INSERT INTO Деталь VALUES (14, N'Повітряний фільтр', 49.00,N'Китай'); 
+INSERT INTO Деталь VALUES (15, N'Амортизатор', 201.00,N'Японія'); 
+ROLLBACK;
+
---
+++ Лаб_12/Lab_12_Trigger.sql
@@ -0,0 +1,25 @@
+﻿USE Ремонт_автомобілів
+GO
+CREATE TRIGGER Del_Insert
+ON Клієнт_1
+AFTER DELETE
+AS
+INSERT INTO Клієнт_2
+SELECT*
+FROM DELETED;
+GO
+
+CREATE TRIGGER UD_Del
+ON Клієнт_2
+AFTER UPDATE
+AS
+DELETE FROM Клієнт_1 WHERE Клієнт_1.Місто=(SELECT Місто FROM DELETED);
+GO
+
+CREATE TRIGGER Insert_Insert
+ON Клієнт_1
+AFTER INSERT
+AS
+INSERT INTO Клієнт_2([Код клієнта],Прізвище,[Ім’я],Місто,Вулиця,Телефон)
+SELECT [Код клієнта],Прізвище,[Ім’я],Місто,Вулиця,Телефон
+FROM inserted;
