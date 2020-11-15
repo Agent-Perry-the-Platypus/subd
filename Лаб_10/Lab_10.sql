604999542f8a90f4406f64a2f427caacbb56943e
Agent-Perry-the-Platypus <72860524+Agent-Perry-the-Platypus@users.noreply.github.com>
2020-11-15 19:10:56 +0200

    процедури та функції

---
+++ Лаб_10/Lab_10.sql
@@ -0,0 +1,27 @@
+﻿use Ремонт_автомобілів;
+GO
+CREATE PROCEDURE Procedure_0 AS
+SELECT Прізвище,[Ім’я],Посада,Телефон
+FROM Працівник;
+GO
+EXEC Procedure_0;
+GO;
+
+CREATE FUNCTION FUNCTION0(@cost money, @count as int)
+RETURNS money
+AS
+	BEGIN
+	if(@cost<0 and @count<0)
+		return 0
+	Return (@cost*@count)
+END;
+GO
+
+Select[Код замовлення], dbo.FUNCTION0(Вартість, [Кількість деталей]) as [Загальна вартість]
+FROM Замовлення
+INNER JOIN Деталь ON Замовлення.[Код деталі]= Деталь.[Код деталі];
+
+
+Select [Код замовлення],Вартість*[Кількість деталей] as [Загальна вартість]
+FROM Замовлення
+INNER JOIN Деталь ON Замовлення.[Код деталі]= Деталь.[Код деталі]
